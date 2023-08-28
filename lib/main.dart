1d4c045f2ee19c61c5502f56ec9096e744712fd9
renreiii <rendi.raihan023@gmail.com>
2023-08-28 14:09:09 +0700

    added navbar icons

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,86 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    double screenWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           color: AppColors.baseBackgroundColor,
-          padding: EdgeInsets.all(width * 0.01),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 1.0),
-            ),
+          padding: EdgeInsets.all(screenWidth * 0.01),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                width: 75,
+                height: 1000,
+                decoration: BoxDecoration(
+                  color: AppColors.mainColorPurple,
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: SvgPicture.asset(
+                          'assets/navigation-bar/icons/nav-side-logo.svg',
+                          width: 36,
+                        ),
+                      ),
+                      Container(
+                        height: 250,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/navigation-bar/icons/nav-side-home.svg',
+                              width: 24,
+                            ),
+                            SvgPicture.asset(
+                              'assets/navigation-bar/icons/nav-side-pos-pembayaran.svg',
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 48 + 32.5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/navigation-bar/icons/nav-side-cog.svg',
+                              width: 24,
+                            ),
+                            SvgPicture.asset(
+                              'assets/navigation-bar/icons/nav-side-logout.svg',
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 60,
+                top: 100,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: AppColors.baseWhite,
+                    shape: BoxShape.circle,
+                  ),
+                  child: SvgPicture.asset(
+                    'assets/navigation-bar/icons/arrow-chevron-right.svg',
+                    width: 8,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
