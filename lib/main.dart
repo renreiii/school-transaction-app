3ac21e99782febdf4c4001049ee7ec33f78004d0
renreiii <rendi.raihan023@gmail.com>
2023-08-28 15:47:40 +0700

    renewing the icons

--- lib/main.dart
+++ lib/main.dart
@@ -13,83 +13,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           color: AppColors.baseBackgroundColor,
           padding: EdgeInsets.all(screenWidth * 0.01),
-          child: Stack(
-            clipBehavior: Clip.none,
+          child: Row(
             children: [
-              Container(
-                width: 75,
-                height: 1000,
-                decoration: BoxDecoration(
-                  color: AppColors.mainColorPurple,
-                  borderRadius: BorderRadius.circular(0),
-                ),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: SvgPicture.asset(
-                          'assets/navigation-bar/icons/nav-side-logo.svg',
-                          width: 36,
-                        ),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    width: screenWidth * 0.05,
+                    height: screenHeight,
+                    decoration: BoxDecoration(
+                      color: AppColors.mainColorPurple,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: SvgPicture.asset(
+                              'assets/icons/nav-side-logo.svg',
+                              width: 36,
+                            ),
+                          ),
+                          Container(
+                            height: 250,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/icons/nav-side-home.svg',
+                                  width: 24,
+                                ),
+                                SvgPicture.asset(
+                                  'assets/icons/nav-side-pos-pembayaran.svg',
+                                  width: 24,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 48 + 32.5,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/icons/nav-side-cog.svg',
+                                  width: 24,
+                                ),
+                                SvgPicture.asset(
+                                  'assets/icons/nav-side-logout.svg',
+                                  width: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        height: 250,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SvgPicture.asset(
-                              'assets/navigation-bar/icons/nav-side-home.svg',
-                              width: 24,
-                            ),
-                            SvgPicture.asset(
-                              'assets/navigation-bar/icons/nav-side-pos-pembayaran.svg',
-                              width: 24,
-                            ),
-                          ],
-                        ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 60,
+                    top: 100,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: AppColors.baseWhite,
+                        shape: BoxShape.circle,
                       ),
-                      Container(
-                        height: 48 + 32.5,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SvgPicture.asset(
-                              'assets/navigation-bar/icons/nav-side-cog.svg',
-                              width: 24,
-                            ),
-                            SvgPicture.asset(
-                              'assets/navigation-bar/icons/nav-side-logout.svg',
-                              width: 24,
-                            ),
-                          ],
-                        ),
+                      child: SvgPicture.asset(
+                        'assets/icons/arrow-chevron-right.svg',
+                        color: AppColors.mainColorPurple,
+                        width: 8,
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              Positioned(
-                left: 60,
-                top: 100,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: AppColors.baseWhite,
-                    shape: BoxShape.circle,
-                  ),
-                  child: SvgPicture.asset(
-                    'assets/navigation-bar/icons/arrow-chevron-right.svg',
-                    width: 8,
-                  ),
-                ),
+                ],
               ),
             ],
           ),
