4b1775e94af3d84c42c9ebad2874837ddda8288a
renreiii <rendi.raihan023@gmail.com>
2023-08-28 08:45:52 +0700

    setup asset and packages

---
+++ lib/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color mainColorPurple = Color(0xFF6F6AF8);
+  static const Color baseBackgroundColor = Color(0xFFF8F9Fb);
+  static const Color baseWhite = Color(0xFFFFFFFF);
+  static const Color iris = Color(0xFFF2F3FF);
+  static const Color notPureWhite = Color(0xFFF1F6F9);
+  static const Color disabled = Color(0xFF919191);
+  static const Color secondaryText = Color(0xFF919191);
+  static const Color label = Color(0xFF616161);
+  static const Color primaryText = Color(0xFF393E46);
+  static const Color gray = Color(0xFFF6F7F9);
+  static const Color titleColor = Color(0xFF7A808D);
+}
